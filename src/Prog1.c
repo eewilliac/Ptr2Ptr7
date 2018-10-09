661b9149d2995351eeeb643e7c3ed78663ca9f6a
ewc <eewilliac@gmail.com>
2018-10-09 10:52:10 -0400

    commiting to local

--- src/Prog1.c
+++ src/Prog1.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+int count_lines();
+
 int main(){
-    printf("Git configured");
+    int line_count=count_lines();
+    printf("linecount %d",line_count);
+}
+
+int count_lines(){
+    FILE *fp=fopen("./names.txt","r");
+    if(fp==NULL){
+        return -1;
+    }
+    int line_count=0;
+    char curr_char=fgetc(fp);
+    while(curr_char!=EOF){
+        if(curr_char=='\n'){
+            line_count++;
+        }
+        curr_char=fgetc(fp);
+    }
+    return line_count;
 }
